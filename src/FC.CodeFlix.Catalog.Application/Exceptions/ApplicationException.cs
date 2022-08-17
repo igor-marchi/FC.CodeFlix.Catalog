f51416303dcc8154ee27e0432a694939aba9358f
Igor Marchi <igoraugustomarchi@hotmail.com>
2022-08-16 22:52:08 -0300

    Criada exception

---
+++ src/FC.CodeFlix.Catalog.Application/Exceptions/ApplicationException.cs
@@ -0,0 +1,7 @@
+﻿namespace FC.CodeFlix.Catalog.Application.Exceptions;
+
+public class ApplicationException : Exception
+{
+    public ApplicationException(string? message) : base(message)
+    { }
+}
