800770f320b31a3e77f67033630d7a23d483d416
Igor Marchi <igoraugustomarchi@hotmail.com>
2022-08-16 22:13:15 -0300

    criado testes e implementação do getCategory

---
+++ src/FC.CodeFlix.Catalog.Application/UseCase/Category/GetCategory/GetCategory.cs
@@ -0,0 +1,21 @@
+﻿using FC.CodeFlix.Catalog.Domain.Repository;
+using MediatR;
+
+namespace FC.CodeFlix.Catalog.Application.UseCase.Category.GetCategory;
+
+public class GetCategory : IRequestHandler<GetCategoryInput, GetCategoryOutput>
+{
+    private readonly ICategoryRepository _categoryRepository;
+
+    public GetCategory(ICategoryRepository categoryRepository)
+    {
+        _categoryRepository = categoryRepository;
+    }
+
+    public async Task<GetCategoryOutput> Handle(GetCategoryInput request, CancellationToken cancellationToken)
+    {
+        var category = await _categoryRepository.Get(request.Id, cancellationToken);
+
+        return GetCategoryOutput.FromCategory(category);
+    }
+}
---
+++ tests/FC.CodeFlix.Catalog.Tests/Application/GetCategory/GetCategoryTestFixture.cs
@@ -0,0 +1,39 @@
+﻿using FC.CodeFlix.Catalog.Domain.Entity;
+using FC.CodeFlix.Catalog.Domain.Repository;
+using FC.CodeFlix.Catalog.Tests.Common;
+using Moq;
+
+namespace FC.CodeFlix.Catalog.Tests.Application.GetCategory;
+
+[CollectionDefinition(nameof(GetCategoryTestFixture))]
+public class GetCategoryTestFixtureCollection : ICollectionFixture<GetCategoryTestFixture>
+{ }
+
+public class GetCategoryTestFixture : BaseFixture
+{
+    public string GetValidCategoryName()
+    {
+        var categoryName = "";
+        while (categoryName.Length < 3)
+            categoryName = Faker.Commerce.Categories(1)[0];
+        if (categoryName.Length > 255)
+            categoryName = categoryName[..255];
+
+        return categoryName;
+    }
+
+    public string GetValidCategoryDescription()
+    {
+        var categoryDescription = Faker.Commerce.ProductDescription();
+
+        if (categoryDescription.Length > 10_000)
+            categoryDescription = categoryDescription[..10_000];
+
+        return categoryDescription;
+    }
+
+    public Category GetValidCategory()
+        => new(GetValidCategoryName(), GetValidCategoryDescription());
+
+    public Mock<ICategoryRepository> GetRepositoryMock() => new();
+}
