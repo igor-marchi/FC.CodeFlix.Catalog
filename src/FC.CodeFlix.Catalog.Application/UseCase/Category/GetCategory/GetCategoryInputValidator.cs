534e8ddb059b90426309be3830006273c3f15fd6
Igor Marchi <igoraugustomarchi@hotmail.com>
2022-09-19 22:17:38 -0300

    create validator

---
+++ src/FC.CodeFlix.Catalog.Application/UseCase/Category/GetCategory/GetCategoryInputValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace FC.CodeFlix.Catalog.Application.UseCase.Category.GetCategory;
+
+public class GetCategoryInputValidator : AbstractValidator<GetCategoryInput>
+{
+    public GetCategoryInputValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty();
+    }
+}
