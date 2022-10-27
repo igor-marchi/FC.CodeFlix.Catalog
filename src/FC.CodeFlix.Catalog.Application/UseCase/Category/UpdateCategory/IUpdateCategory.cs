dcf09585f95a59df5ee99ddc751c38e71182b8ff
Igor Marchi <igoraugustomarchi@gmail.com>
2022-10-26 21:03:24 -0300

    create updateCategory use case

---
+++ src/FC.CodeFlix.Catalog.Application/UseCase/Category/UpdateCategory/IUpdateCategory.cs
@@ -0,0 +1,6 @@
+﻿using FC.CodeFlix.Catalog.Application.UseCase.Category.Common;
+using MediatR;
+
+namespace FC.CodeFlix.Catalog.Application.UseCase.Category.UpdateCategory;
+public interface IUpdateCategory : IRequestHandler<UpdateCategoryInput, CategoryModelOutput>
+{ }
---
+++ src/FC.CodeFlix.Catalog.Application/UseCase/Category/UpdateCategory/UpdateCategory.cs
@@ -0,0 +1,32 @@
+﻿using FC.CodeFlix.Catalog.Application.Interfaces;
+using FC.CodeFlix.Catalog.Application.UseCase.Category.Common;
+using FC.CodeFlix.Catalog.Domain.Repository;
+
+namespace FC.CodeFlix.Catalog.Application.UseCase.Category.UpdateCategory;
+public class UpdateCategory : IUpdateCategory
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public UpdateCategory(ICategoryRepository categoryRepository, IUnitOfWork unitOfWork)
+    {
+        _categoryRepository = categoryRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task<CategoryModelOutput> Handle(UpdateCategoryInput request, CancellationToken cancellationToken)
+    {
+        var category = await _categoryRepository.Get(request.Id, cancellationToken);
+        category.Update(request.Name, request.Description);
+
+        if (request.IsActive == category.IsActive) return CategoryModelOutput.FromCategory(category);
+
+        if (request.IsActive) category.Activate();
+        else category.Deactivate();
+
+        await _categoryRepository.Update(category, cancellationToken);
+        await _unitOfWork.Commit(cancellationToken);
+
+        return CategoryModelOutput.FromCategory(category);
+    }
+}
